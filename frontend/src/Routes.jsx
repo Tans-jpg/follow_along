4fecd0461f1789a6ae1dd31ed0282ebf40989f64
Tans-jpg <tanvi.agarwal@kalvium.community>
2025-03-11 12:44:36 +0530

    m22,23,24,25,26

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -4,7 +4,10 @@ import Home from "./pages/Home.jsx";
 import CreateProduct from './pages/createProduct.jsx';
 import MyProducts from "./pages/myProducts.jsx";
 import Cart from "./pages/cart.jsx";
-import ProductDetails from "./pages/productdetails.jsx";
+import ProductDetails from "./pages/productDetails.jsx";
 import Profile from "./pages/profile.jsx";
 import CreateAddress from "./pages/createAddress.jsx";
-export {LoginPage,SignupPage,Home,CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress};
+import SelectAddress from "./pages/SelectAddress.jsx";
+import OrderConfirmation from "./pages/OrderConfirmation.jsx";
+
+export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress, SelectAddress, OrderConfirmation };
